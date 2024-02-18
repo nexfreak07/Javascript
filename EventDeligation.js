15ea9630a0ca6953dd93573d0fdae9fe4ba638b5
Akash Dasgupta <akashakxy@gmail.com>
2024-02-19 04:59:02 +0530

    Add files via upload

---
+++ EventDeligation.js
@@ -0,0 +1,9 @@
+document.getElementById('categories').addEventListener('click', (e)=>{
+    console.log(e.target)
+    
+    if(e.target.tagName == 'LI'){
+        
+        
+        // window.location.href = "/" + e.target.id;
+    }
+})
